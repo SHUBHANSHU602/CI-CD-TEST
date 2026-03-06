22133133b57fdc79c1d133b2847f32bd9cce3b44
Shubhanhsu Singh <shubhanshusingh602@gmail.com>
2026-03-06 22:28:04 +0530

    Fix formatting issues in palindrome tests

--- tests/palindrome.test.js
+++ tests/palindrome.test.js
@@ -1,10 +1,11 @@
 const isPalindrome = require("../src/palindrome");
 
 test("madam should be palindrome", () => {
-  expect(isPalindrome("madam)).toBe(true);
+  expect(isPalindrome("madam")).toBe(true);
 });
 
 test("hello should not be palindrome", () => {
   expect(isPalindrome("hello")).toBe(false);
 
 });
+
