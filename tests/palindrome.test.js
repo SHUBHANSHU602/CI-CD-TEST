056dda8572578021f32c3375ebd3fc37bc57bde6
SHUBHANSHU602 <shubhanshusingh602@gmail.com>
2026-03-06 22:23:26 +0530

    ci/cd

---
+++ tests/palindrome.test.js
@@ -0,0 +1,9 @@
+const isPalindrome = require("../src/palindrome");
+
+test("madam should be palindrome", () => {
+  expect(isPalindrome("madam")).toBe(true);
+});
+
+test("hello should not be palindrome", () => {
+  expect(isPalindrome("hello")).toBe(false);
+});
